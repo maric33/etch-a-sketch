456be4ade1fbbca5c0f52bd36664ab37fe650cf8
maric33 <161638899+maric33@users.noreply.github.com>
2024-03-07 17:11:08 +0100

    Created 16x16 grid through Javascript

--- script/script.js
+++ script/script.js
@@ -6,14 +6,28 @@ mainContainer.id = 'mainContainer';
 const rowContainer = document.createElement('div');
 rowContainer.id = 'rowContainer';
 
-// const smallContainer = document.createElement('div');
-// smallContainer.id = 'smallContainer';
-
-
 document.body.appendChild(mainContainer);
-mainContainer.appendChild(rowContainer);
 
-for ( let i = 0; i <= 16; i++){
-    let element = document.createElement('div');
-    rowContainer.appendChild(element);
+for (let i = 0; i <=15; i++){
+    let rowContainer = document.createElement('div');
+    rowContainer.id = 'rowContainer';
+    mainContainer.appendChild(rowContainer);
+        for ( let i = 0; i <= 15; i++){
+        let smallContainer = document.createElement('div');
+        smallContainer.id= 'smallContainer';
+        rowContainer.appendChild(smallContainer);
 };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
